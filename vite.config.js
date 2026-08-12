38a8b5813497e72ab82595a10ca91b79137965fb
Diego336s <sanabriadiego336@gmail.com>
2026-08-11 22:18:54 -0500

    Eliminar archivo de estilo de inicio de sesión de autenticación para simplificar la configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/css/auth/login.css',
                 'resources/css/usuarios/index.css',
                 ...auth,
                 'resources/css/usuarios/view.css'
